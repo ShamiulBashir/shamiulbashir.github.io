5c85c8c137987efcf4849f1f4e9ac8dc0f5a2e7e
ShamiulBashir <bashir15-8467@diu.edu.bd>
2023-06-19 14:07:14 +0600

    first commit

---
+++ assets/js/preloader.js
@@ -0,0 +1,15 @@
+/*
+	Document	: Preloader Activator
+	Type		: Java Script (JS)
+	Author		: A.T.M ShamiulBashir
+	Website		: http://shamiulbashir.github.io
+	Description	: © 2020 - 2022 A.T.M ShamiulBashir.
+*/
+
+
+
+/* Begin */
+
+//document.write('<div id="loader-wrapper"><h4 class="loader-title">Loading...</h4><div id="loader"></div><div class="loader-section section-left"></div><div class="loader-section section-right"></div></div>');
+
+/* The End */
---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+/*
+	Document	: Custom Script
+	Type		: Java Script (JS)
+	Author		: A.T.M Shamiul Bashir
+	Website		: http://shamiul.engineer
+	Description	: © 2020 - 2022 Shamiul Bashir.
+*/
+
+
+
+/* Header */
+document.getElementById("header-title-sam").innerHTML = "A.T.M Shamiul Bashir";
+
+
+/* Footer */
+document.getElementById("current_year").innerHTML = new Date().getFullYear();
